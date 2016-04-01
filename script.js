cd61b92ec6cf672e441f14f095ecf1d41c5cdf3a
AmaranthNebula <nikitagia@comcast.net>
2016-04-01 13:25:23 -0700

    started script file; search event handler setup

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   
+   
+   //event handlers
+   // search button
+   $("#search a").click(function(e) {
+     e.preventDefault();
+     
+     //text in search/input box
+     var input = $("#search input").val();
+     
+     //use this input and call function to submit query and show results
+   }); 
+    
+});
