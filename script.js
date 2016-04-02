7e8ef08db841039163173d8eb6d9bd39616aabe8
AmaranthNebula <nikitagia@comcast.net>
2016-04-01 21:04:22 -0700

    search button complete, search results, and GUI finisehd

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    var startURL = "https://en.wikipedia.org/wiki/api.php?action=query&format=json&list=search&srsearch=";
-    var endURL = "callback=?";
-   var searchURL;
+    var startURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
+    var endURL = "&callback=?";
+   var searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=rock&callback=?";
    
    
  //getResults();
    function getResults() {
-       $.getJSON("rockResults.json", function(json) {
+       $.getJSON(searchURL, function(json) {
            var resultLength = json.query.search.length;
            //loop through the ten results
+           $("#results").html("");
            for (var i =0; i < resultLength; i++) {
                 var title = JSON.stringify(json.query.search[i].title).substr(1, json.query.search[i].title.length);
                 var articleURL = "https://en.wikipedia.org/wiki/" + title.replace(/\s/g, "_");
@@ -31,24 +32,17 @@ $(document).ready(function() {
    
    //resize handlers
    function repositionFooter() {
-       console.log("docusmnet: " + $(document).outerHeight());
-       console.log("window: " + $(window).outerHeight());
-           if ($(document).outerHeight() > $(window).outerHeight()) {
-            //    var resultsHeight = $(window).outerHeight() - $("header").outerHeight() - $("footer").outerHeight();
-            //   console.log(resultsHeight);
-            //    $("#results").css("height", "resultsHeight" + "px");
-            var containerHeight = $("header").outerHeight() + $("footer").outerHeight() + $("#results").outerHeight();
-               console.log("containerHeight: " + containerHeight);
-               $("#container").css("height", containerHeight + "px");
-               $("body").css("height", containerHeight + "px");
-               
-               $("footer").css("position", "static");
-           }
-           else {
-               $("footer").css("position", "absolute");
-               $("footer").css("bottom", "0");
-               $("footer").css("left", "0");
-           }       
+        if ($(document).outerHeight() > $(window).outerHeight()) {
+        var containerHeight = $("header").outerHeight() + $("footer").outerHeight() + $("#results").outerHeight();
+            $("#container").css("height", containerHeight + "px");
+            $("body").css("height", containerHeight + "px");
+            $("footer").css("position", "static");
+        }
+        else {
+            $("footer").css("position", "absolute");
+            $("footer").css("bottom", "0");
+            $("footer").css("left", "0");
+        }       
    }
    $(window).resize(function() {
       repositionFooter(); 
@@ -64,14 +58,29 @@ $(document).ready(function() {
     //if there is text in the searchbox
      if (input.length > 0) {
          // create url to send to api and retrieve results
-         searchURL = startURL + input + endURL;
+        searchURL = startURL + input + endURL;
          // call function to generate results and update GUI 
-         //UNCOMMENT THIS LATER getResults();
-
-     console.log(input);
-     //use this input and call function to submit query and show results
+         getResults();
      }
-    getResults();
+
+
    }); 
+   
+   //key event handlers
+   $("#search input").keyup(function(event){
+    if(event.keyCode == 13){
+        //text in search/input box
+        var input = $("#search input").val();
+    
+        //if there is text in the searchbox
+        if (input.length > 0) {
+        // create url to send to api and retrieve results
+        searchURL = startURL + input + endURL;
+        // call function to generate results and update GUI 
+            getResults();
+        }
+    }
+});
+
     
 });
